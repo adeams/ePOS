72a8f84c313bf62693236482473eff9161e5c924
adeams <lookmun@gmail.com>
2017-09-21 17:17:24 +0700

    test edit salepage

--- imports/ui/components/salesPage/salesPage.js
+++ imports/ui/components/salesPage/salesPage.js
@@ -4,12 +4,30 @@ import './salesPage.html';
 
 Template.salesPage.onCreated(function () {
   Meteor.subscribe('links.all');
+  $(document).ready(function(){
+       console.log("$(document).ready(function()")
+      //$(".button-collapse").sideNav();
+      $('select').material_select();
+      //Materialize.updateTextFields();
+  });
 });
 
 Template.salesPage.helpers({
   links() {
     return Links.find({});
   },
+});
+
+Template.salesPage.onRendered(function(){
+  console.log('Template.salesPage.onRendered');
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year,
+    today: 'Today',
+    clear: 'Clear',
+    close: 'Ok',
+    closeOnSelect: false // Close upon selecting a date,
+  });
 });
 
 Template.salesPage.events({
@@ -31,10 +49,6 @@ Template.salesPage.events({
   },
 });
 
-$(document).ready(function(){
-    // console.log("wwwwwwwwwww")
-    //$(".button-collapse").sideNav();
-    $('select').material_select();
-});
+
 
 
